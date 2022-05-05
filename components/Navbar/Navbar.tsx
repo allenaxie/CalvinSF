9d8cf5cad4fdcffd94db2c22f438ced486275947
allenaxie <allenaxie1@gmail.com>
2022-05-04 20:34:57 -0700

    Create active state for nav bar links.

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,12 +1,44 @@
+import { useState } from 'react';
 import classes from './Navbar.module.scss';
 import Image from 'next/image';
+import Link from 'next/link';
 
 const Navbar = () => {
+  const [active, setActive] = useState('')
+
+  const navTabs = [
+    'Home',
+    'Sold',
+    'Buyer Consultation',
+    'About Me',
+  ]
+
+  const handleClick = (item: string) => {
+    console.log('item', item)
+    setActive(item);
+    console.log('active', active)
+  }
+
   return (
     <nav className={classes.main}>
-      <div className={classes.logoText}>
-        <h3 >Calvin Luo</h3>
+      <div className={classes.logoContainer}>
+        <h3 className={classes.logoText}>Calvin Luo</h3>
       </div>
+      <ul className={classes.navbarLinks}>
+        {navTabs.map((item, index) => (
+          <Link
+            href='/'
+          >
+            <li
+              onClick={() => handleClick(item)}
+              key={`link-${item}`}
+              className={active === item ? `${classes.activeLink}` : ''}
+            >
+              {item}
+            </li>
+          </Link>
+        ))}
+      </ul>
     </nav>
   )
 }
