ea8d2a7b3d60f593ebb3856602e17dd86790b3a2
allenaxie <allenaxie1@gmail.com>
2022-05-06 02:17:25 -0700

    Add background image to header.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import classes from '../styles/Home.module.scss';
-import {Col, Row} from 'antd';
+import { Col, Row } from 'antd';
 
 const Home: NextPage = () => {
   return (
@@ -13,6 +13,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className={classes.main}>
+
         <section className={classes.headerSection}>
           <div className={classes.headerTextContainer}>
             <span className={classes.primaryText}>Calvin Luo</span>
@@ -23,23 +24,42 @@ const Home: NextPage = () => {
           </div>
         </section>
 
-        <Row className={classes.aboutRow}>
+        <Row className={classes.aboutContainer}>
           <Col
-            lg={{span:8}}
+            xs={{ span: 24 }}
+            lg={{ span: 8 }}
+            className={classes.aboutCol}
           >
             image
           </Col>
-          <Col 
-            className={classes.aboutHeading}
-            lg={{span:8}}
+          <Col
+            className={`${classes.aboutTextContainer} ${classes.aboutCol}`}
+            xs={{ span: 24 }}
+            lg={{ span: 8 }}
           >
-            <span>About Me</span>
+            <div className={classes.aboutHeading}>
+              <span>About Me</span>
+            </div>
+            <div className={classes.aboutContent}>
+              <span>
+                I have overseen numerous transactions as well as manage workflows around home buying and selling. Coupled with my knowledge of the city, and my experiences of having lived here all my life, I have the enthusiasm and desire to ensure your home buying/selling process flows as smoothly as possible.
+                <br/><br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+              </span>
+              <button className={classes.aboutBtn}>Learn More</button>
+            </div>
           </Col>
           <Col
-            lg={{span:8}}
+            xs={{ span: 24 }}
+            lg={{ span: 8 }}
+            className={classes.aboutCol}
           >
             image
           </Col>
+        </Row>
+
+        <Row className={classes.propertiesContainer}>
+          <button className={classes.propertiesBtn}>Sold Properties</button>
         </Row>
       </main>
     </div>
